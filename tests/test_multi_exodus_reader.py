b4189455a8fcb1d58e8833e78b04cfffb62d932b
Chaitanya Bhave <65254651+chaibhave@users.noreply.github.com>
2025-08-17 10:07:05 -0600

    Rename example script and add unit test

---
+++ tests/test_multi_exodus_reader.py
@@ -0,0 +1,26 @@
+import pytest
+np = pytest.importorskip("numpy")
+import MultiExodusReader as mer
+
+
+class DummyExodusReader:
+    def __init__(self, file_name):
+        self.times = np.array([0.0, 1.0])
+        self.x = np.array([[0.0], [1.0]])
+        self.y = np.array([[0.0], [0.0]])
+        self.z = np.array([[0.0], [0.0]])
+        self.dim = 1
+
+    def get_var_values(self, var_name, idx):
+        return np.array([idx, idx + 1])
+
+
+def test_get_data_at_time(monkeypatch):
+    monkeypatch.setattr(mer.glob, "glob", lambda pattern: ["dummy.e"])
+    monkeypatch.setattr(mer.ExodusReader, "ExodusReader", DummyExodusReader)
+    mr = mer.MultiExodusReader("dummy.e")
+    x, y, z, c = mr.get_data_at_time("c_Cr", 1.0)
+    assert x.shape == (2, 1)
+    assert y.shape == (2, 1)
+    assert z.shape == (2, 1)
+    assert np.array_equal(c, np.array([1, 2]))
